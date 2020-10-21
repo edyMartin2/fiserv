e92027ac230e32da5306e0372c5a603d1f66219c
edyMartin2 <evalerio.sdc@hotmail.com>
2020-10-21 14:11:18 -0500

    v1.0.2

--- app/src/main/java/com/gonzmor/fiserv/MainActivity.java
+++ app/src/main/java/com/gonzmor/fiserv/MainActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,66 +26,136 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import cz.msebera.android.httpclient.client.HttpResponseException;
+import cz.msebera.android.httpclient.entity.mime.Header;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView mylist ;
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter adapter;
+    Button btn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String data = getData();
-        TextView x = (TextView) findViewById(R.id.z);
-        x.setText(data);
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        mylist = findViewById(R.id.mylist);
+        mylist.setAdapter(adapter);
 
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //create instance
+                //Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+                RestClient cliente = new RestClient(getApplicationContext());
+
+                try{
+                    Toast.makeText(MainActivity.this, "sssss", Toast.LENGTH_SHORT).show();
+                    //Build JSON
+                    JSONObject saleTransaction = new JSONObject();
+                    saleTransaction.put("requestType","PaymentCardSaleTransaction");
+                    saleTransaction.put("transactionAmount",
+                            (new JSONObject())
+                                    .put("total","1.00")
+                                    .put("currency","MXN")
+                    );
+                    saleTransaction.put("paymentMethod",
+                            (new JSONObject()).put("paymentCard",
+                                    (new JSONObject())
+                                            .put("number","5579xxxxxxxx12")
+                                            .put("securityCode","123")
+                                            .put("expiryDate",
+                                                    (new JSONObject())
+                                                            .put("month","12")
+                                                            .put("year","25")
+                                            )
+                            )
+                    );
+                    //send POST HTTP REQUEST
+                    cliente.post("payments", saleTransaction, new JsonHttpResponseHandler(){
+
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            super.onSuccess(statusCode, (cz.msebera.android.httpclient.Header[]) headers, response);
+                            //handle success when response is a single object
+                            Toast.makeText(MainActivity.this, "err4", Toast.LENGTH_SHORT).show();
+                            Log.d("SuccessObj", response.toString());
+                        }
+
+
+                        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                            super.onSuccess(statusCode, (cz.msebera.android.httpclient.Header[]) headers, response);
+                            //handle success when response is an array of objects
+                            Toast.makeText(MainActivity.this, "er3", Toast.LENGTH_SHORT).show();
+                            Log.d("SuccessArr", response.toString());
+                        }
+
+
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                            super.onFailure(statusCode, (cz.msebera.android.httpclient.Header[]) headers, throwable, errorResponse);
+                            //handle error when response is an array of objects
+                            Toast.makeText(MainActivity.this, "err2", Toast.LENGTH_SHORT).show();
+                            Log.d("ErrorArr", errorResponse.toString());
+                        }
+
+
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            super.onFailure(statusCode, (cz.msebera.android.httpclient.Header[]) headers, throwable, errorResponse);
+                            //handle success when response is a single object
+                            Toast.makeText(MainActivity.this, "err1", Toast.LENGTH_SHORT).show();
+                            Log.d("ErrorObj", errorResponse.toString());
+                        }
+                    });
+                    //Toast.makeText(MainActivity.this, "sssss", Toast.LENGTH_SHORT).show();
+                }catch (HttpResponseException ex){
+                    Toast.makeText(MainActivity.this, "uno", Toast.LENGTH_SHORT).show();
+                    Log.d("HttpResponseException", ex.getMessage());
+                }
+                catch (JSONException ex){
+                    Toast.makeText(MainActivity.this, "dos", Toast.LENGTH_SHORT).show();
+                    Log.d("JSONException", ex.getMessage());
+                }
+                catch (Exception ex){
+                    Toast.makeText(MainActivity.this, "tres", Toast.LENGTH_SHORT).show();
+                    Log.d("Exception", ex.getMessage());
+                }
+
+            }
+        });
     }
+    private void getPost(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        fiservInterface fi = retrofit.create(fiservInterface.class);
 
-    public String getData(){
-        String baseUrl = "https://httpbin.org/get";
-        URL url = null;
-        String inputLine = "";
-        StringBuffer res;
-        String json = "";
-        JSONArray dataJson = null;
-        String dataReq = "ssss";
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        Call<List<POST>> call = fi.getPost();
 
-        HttpURLConnection connection ;
+        call.enqueue(new Callback<List<POST>>() {
+            @Override
+            public void onResponse(Call<List<POST>> call, Response<List<POST>> response) {
+                for (POST post: response.body() ){
+                    list.add(post.getTitle());
+                }
 
-        try {
-            url = new URL(baseUrl);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            BufferedReader read = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            res = new StringBuffer();
-            while ((inputLine = read.readLine()) != null){
-                res.append(inputLine);
+                adapter.notifyDataSetChanged();
             }
 
-            json = res.toString();
+            @Override
+            public void onFailure(Call<List<POST>> call, Throwable t) {
 
-            dataJson = new JSONArray(json);
-
-            for (int i = 0 ; i < dataJson.length(); i++){
-                JSONObject obj = dataJson.getJSONObject(i);
-
-                dataReq += "indice"+i+ obj.optString("args") + "\n";
             }
-            return  dataReq;
-        } catch (MalformedURLException e) {
-            return " fgggh" + e.toString();
-        } catch (IOException e) {
-            return "2" +e.toString();
-        } catch (JSONException e) {
-            return "hji3" +e.toString();
-        }
-
-
-
+        });
     }
 
 
---
+++ app/src/main/java/com/gonzmor/fiserv/POST.java
@@ -0,0 +1,30 @@
+package com.gonzmor.fiserv;
+
+public class POST {
+
+    int userId;
+    int id;
+    String title;
+    String body;
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+
+
+
+}
